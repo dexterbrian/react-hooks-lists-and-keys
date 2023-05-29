ac75a6a3ac76340e92c9abef7ed435f6a35adcea
Brian Waweru <brianwaweru627@gmail.com>
2023-05-29 11:48:07 +0300

    Removed file

--- src/components/TextInput.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-function TextInput({ inputType, inputId, inputPlaceholder }) {
-    return (
-        <input id={ inputId } type={ inputType } placeholder={ inputPlaceholder }/>
-    );
-}
-
-export default TextInput;
